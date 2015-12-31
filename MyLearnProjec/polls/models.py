7e30fca3a6efc93f1ca9b5e86c9674cbbd10eaaf
Breazu Adrian <breazuadrian@gmail.com>
2015-12-31 13:33:43 +0200

    lesson 2 - step 4&5 play with the shell and add Questions to the admin page

--- MyLearnProjec/polls/models.py
+++ MyLearnProjec/polls/models.py
@@ -1,12 +1,20 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Question(models.Model):
     # restrict the field lenght to 200 characters
     question_text = models.CharField(max_length=200)
-    #get the human-readable name date published
+    # get the human-readable name date published
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -16,3 +24,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     # set the default value to 0
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
