2097e91d71ed4e1a3a56067d44e3ee14a0e5cd4f
Breazu Adrian <breazuadrian@gmail.com>
2016-01-01 17:07:40 +0200

    lesson 7 - step 1 customize admin form

--- MyLearnProjec/polls/admin.py
+++ MyLearnProjec/polls/admin.py
@@ -3,4 +3,16 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register((Question, Choice))
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin (admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'],'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
